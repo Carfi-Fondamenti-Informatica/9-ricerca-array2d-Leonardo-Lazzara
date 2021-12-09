03606257388ede0704074ed8805039843aa84239
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-09 22:51:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int find( const char lista[10][20], const char nome[20]) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 20; j++) {
-                if(ista[i][j]!=nome[j] && lista[i][j]!=nome[j]+32 && lista[i][j]!=nome[j]-32){
+                if(ista[i][j]!=nome[j]){
                     break;
                 }else {
                     return i;
