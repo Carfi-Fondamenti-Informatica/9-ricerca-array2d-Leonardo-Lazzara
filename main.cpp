e839390cf9fd74fbd5821d04b67dc3abf082445f
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-09 22:35:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    char a[10][20];
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+     }
+
+    char nome[20];
+        cin >> nome;
+
+    int risultato= find(a,nome);
+    if(risultato==-1){
+        cout << "non presente" << endl;
+    }else{
+        cout << risultato << endl;
+    }
+
+    return 0;
 }
