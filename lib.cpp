00a38abc4cad4a0fc6399ff4d346f4f1b7556bba
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 21:54:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 #include <string.h>
 
-bool risulato(char lista[10][20], char nome[20], int &i) {
+bool posizione(char lista[10][20], char nome[20], int &i) {
         for (int i = 0; i < 10; i++) {
                 if((strcmp(lista[i],nome))==0){
                         return true;
