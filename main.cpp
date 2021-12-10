4c7e095bebc83b486f583b365f36341e8765a9de
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 21:46:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,19 @@
 #include "lib.h"
 using namespace std;
 
-
 int main() {
-    char a[10][20];
-    for (int i = 0; i < 10; i++) {
-        cin >> a[i];
-     }
-
+    char lista[10][20];
     char nome[20];
-        cin >> nome;
+    int x=0
+    for (int i = 0; i < 10; i++) {
+        cin >> lista[i];
+     }
+    cin >> nome;
 
-    int risultato= find(a,nome);
-    if(risultato==-1){
-        cout << "non presente" << endl;
+    if(posizione(lista,nome,x)){
+        cout << x << endl;
     }else{
-        cout << risultato << endl;
+        cout << "non presente" << endl;
     }
-
     return 0;
 }
