944e103ee437c39722f72d9beec89c7fb03ec7b8
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 22:52:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
 #include "lib.h"
-
 using namespace std;
+
 int main() {
     char lista[10][20];
     char nome[1][20];
- 
-    for(int i=0; i<10; i++){
-        for(int j=0; j<20; j++){
-            lista[i][j]=' ';
-        }
-    }
-    for(int i=0; i<20; i++){
-        nome[0][i]=' ';
-    }
-
-    for(int i=0; i<10; i++){
+    int posizione=0;
+    for (int i = 0; i < 10; i++) {
         cin >> lista[i];
     }
     cin >> nome[0];
 
-    int risultato=find(lista,nome);
-    if(risultato==-1){
-        cout<<"non presente"<<endl;
+    if(risultato(lista,nome,posizione)){
+        cout << posizione << endl;
     }else{
-        cout<< risultato <<endl;
+        cout << "non presente" << endl;
     }
-
     return 0;
 }
