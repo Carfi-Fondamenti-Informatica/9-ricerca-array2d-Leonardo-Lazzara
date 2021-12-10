de83e021eae4a3da29bebcfcbb58d54e1e5773b6
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 22:01:35 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+ #ifndef INC_9_RICERCA_ARRAY2D_Leonardo-Lazzara
+ #define INC_9_RICERCA_ARRAY2D_Leonardo-Lazzara
+
 int posizione(char lista[10][20], char nome[20], int &i);
+
+#endif//INC_9_RICERCA_ARRAY2D_Leonardo-Lazzara_LIB_H
