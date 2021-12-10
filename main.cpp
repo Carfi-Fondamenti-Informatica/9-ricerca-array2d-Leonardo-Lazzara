4a6867a6767a9547ccb4dd291d575afd07ee166f
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 21:52:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+int main() {
+    char lista[10][20];
+    char nome[20];
+    int x=0
+    for (int i = 0; i < 10; i++) {
+        cin >> lista[i];
+     }
+    cin >> nome;
+
+    if(posizione(lista,nome,x)){
+        cout << x << endl;
+    }else{
+        cout << "non presente" << endl;
+    }
+    return 0;
+}
