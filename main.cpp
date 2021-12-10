cf95393b70fa6f463350c79f855d7827e33efbd6
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-12-10 22:08:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main() {
     char lista[10][20];
-    char nome[20];
-    int x=0
+    char nome[1][20];
+    int posizione=0
     for (int i = 0; i < 10; i++) {
         cin >> lista[i];
      }
-    cin >> nome;
+    cin >> nome[0];
 
-    if(posizione(lista,nome,x)){
-        cout << x << endl;
+    if(risultato(lista,nome,posizione)){
+        cout << posizione << endl;
     }else{
         cout << "non presente" << endl;
     }
